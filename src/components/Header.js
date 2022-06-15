9bdd5a2a5db261d16437047446b4fbc96c096552
Ahreum0714 <yalin7145@gmail.com>
2022-06-15 17:58:40 +0900

    Header(내비게이션바) 컴포넌트 기본 뼈대 파일 추가

---
+++ src/components/Header.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Header.css";
+
+export default function Header({ isLogin, connectWallet, account }) {
+  return (
+    <div id="Navbar">
+      <nav>
+        <Link to="/">
+          <button>LOGO(main)</button>
+        </Link>
+        <Link to="/list">
+          <button>Explore</button>
+        </Link>
+        <Link to="/create">
+          <button>Create</button>
+        </Link>
+        <Link to="/profile">
+          <button>Profile</button>
+        </Link>
+        <button onClick={() => connectWallet()}>connect to MetaMask</button>
+        {/* // 연결된 계정 주소를 화면에 출력합니다 */}
+        연결된 계정 주소: {account}
+      </nav>
+    </div>
+  );
+}
