34b0c956f1592ad43265eb9c997909979cb2b662
Ahreum0714 <yalin7145@gmail.com>
2022-06-17 01:54:03 +0900

    Pagination 구현 완료 - 한 페이지당 토큰 8개(행2*열4)

--- src/components/NftCardList.js
+++ src/components/NftCardList.js
@@ -1,12 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import NftCard from "./NftCard";
 
-function NftCardList({ nfts, web3 }) {
+function NftCardList({ nfts, web3, limit, page }) {
+  const offset = (page - 1) * limit;
   return (
-    <div className="nft-list">
-      {nfts.map((nft) => (
-        <NftCard nft={nft} key={nft.tokenId} />
-      ))}
+    <div className="nft-list-container">
+      <div className="nft-list">
+        {nfts.slice(offset, offset + limit).map((nft) => (
+          <NftCard nft={nft} key={nft.tokenId} />
+        ))}
+      </div>
     </div>
   );
 }
