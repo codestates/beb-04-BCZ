3ed4b078fc3b1920265486b4272f777bc7ba6200
Ahreum0714 <yalin7145@gmail.com>
2022-06-17 12:46:23 +0900

    Nft Detail View - 레이아웃, css 꾸미기, 뒤로가기 아이콘, 속성 틀

--- src/components/NftCardDetail.js
+++ src/components/NftCardDetail.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useNavigate, useParams } from "react-router-dom";
-
+import { RiArrowLeftCircleFill } from "react-icons/ri";
 // NFT 카드의 상세 페이지
 function NftCardDetail({ nfts, web3, match }) {
   const navigate = useNavigate();
@@ -8,14 +8,21 @@ function NftCardDetail({ nfts, web3, match }) {
   let nft = nfts.find((el) => el.tokenId === Number(tokenId));
 
   return (
-    <div className="nft-detail">
-      <h1> detail </h1>
-      <p>{nft.tokenId}</p>
-      <div>
-        <button onClick={() => navigate(-1)}>go back</button>
+    <div className="nft-detail-container">
+      <div className="nft-detail-back">
+        <RiArrowLeftCircleFill color="#6d5dfe" fontSize="2.5em" onClick={() => navigate(-1)} />
       </div>
-      <div className="nft-detail-img">
-        <img src={nft.tokenURI} alt={`Img${nft.tokenId}`} width={500} />
+      <div className="nft-detail-body">
+        <div className="nft-detail-img">
+          <img src={nft.tokenURI} alt={`Img${nft.tokenId}`} width="100%" />
+        </div>
+        <div className="nft-detail-info">
+          <h1>#{nft.tokenId} NAME</h1>
+          <p>Owned by:</p>
+          <p>Description:</p>
+          <p>Contract Address:</p>
+          <p>Blockchain: Ethereum</p>
+        </div>
       </div>
       {/* <span className="asset-name">{asset.name}</span>
       <span className="asset-description">{asset.description}</span>
